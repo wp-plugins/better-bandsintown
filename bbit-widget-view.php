444dee82dd5769436ca7da9452dce43f9b04ac0d
mrpaplu <mrpaplu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-20 17:27:13 +0000

    The bandname in the widget is a link to the bandsintown page of the band now.
    git-svn-id: https://plugins.svn.wordpress.org/better-bandsintown/trunk@1095323 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bbit-widget-view.php
+++ bbit-widget-view.php
@@ -1,9 +1,11 @@
 <div class="bbit-widget_wrapper">
-    <div class="bandsintown-logo-wrapper">
-        <img class="bandsintown-logo" src="<?php echo plugins_url('images/bandsintown-logo.png', __FILE__ ) ?>" />
-        <div class="bbit-widget-bandname"><?php echo $bandname; ?></div>
-        <div class="clear-both"></div>
-    </div>
+    <a class="bandsintown-logo-link" href="http://www.bandsintown.com/<?php echo $bandname; ?>">
+        <div class="bandsintown-logo-wrapper">
+            <img class="bandsintown-logo" src="<?php echo plugins_url('images/bandsintown-logo.png', __FILE__ ) ?>" />
+            <div class="bbit-widget-bandname"><?php echo $bandname; ?></div>
+            <div class="clear-both"></div>
+        </div>
+    </a>
     <a href="http://www.bandsintown.com" 
        class="bit-widget-initializer"
        data-artist="<?php echo $bandname; ?>" 
